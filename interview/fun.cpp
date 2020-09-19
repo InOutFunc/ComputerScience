47abb13eb03bc67a6f0ac695597b8b86ab3e366e
c00518663 <caoxin24@huawei.com>
2020-09-19 14:42:25 +0800

    [netconf]
    feat/fix/style/ref:
    func:
    input:
    output:
    related:
    
    [issue id]AR.SR.SR.IR.Enterprise.DCI.V1R19C10.062.002.001.002

--- interview/fun.cpp
+++ interview/fun.cpp
@@ -3,8 +3,19 @@
 
 using namespace std;
 
-int func(string s, int k)
+int func(string s)
 {
-    unordered_map<int, int> m;
-    int 
+    unordered_map<char, int> m;
+    int res = 0;
+    int left = 0;
+    for (int i = 0; i < s.size(); i++) {
+        m[s[i]]++;
+        while (m.size() > 2) {
+            if (--m[s[left]] == 0) {
+                m.erase(s[left]);
+            }
+            ++left;
+        }
+        res = max(res, i - left + 1);
+    }
 }
